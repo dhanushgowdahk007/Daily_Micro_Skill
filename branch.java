5f40ca6073bb5238bca79ad9eb1ebe8393b2c961
Dhanush Gowda H K <dhanushgowdahk04@gmail.com>
2025-11-02 14:40:35 +0530

    A file in my_cool_feature (2nd commit)

--- branch.java
+++ branch.java
@@ -1,5 +1,6 @@
 public class branch {
     public static void main(String[] args) {
         System.out.println("This is a sample Java class for demonstrating branching.");
+        System.out.println("This line is added to demonstrate branching.");
     }
 }
